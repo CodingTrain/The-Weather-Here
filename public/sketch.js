f2c1aae6b1df3e066365d9fa7fe7fb62e6bbfb9d
Daniel Shiffman <daniel@shiffman.net>
2019-05-21 18:17:59 -0400

    this is the code from weather here as of video 5-4

---
+++ public/sketch.js
@@ -0,0 +1,43 @@
+// Geo Locate
+let lat, lon;
+if ('geolocation' in navigator) {
+  console.log('geolocation available');
+  navigator.geolocation.getCurrentPosition(async position => {
+    let lat, lon, weather, air;
+    try {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      document.getElementById('latitude').textContent = lat.toFixed(2);
+      document.getElementById('longitude').textContent = lon.toFixed(2);
+      const api_url = `weather/${lat},${lon}`;
+      const response = await fetch(api_url);
+      const json = await response.json();
+      weather = json.weather.currently;
+      air = json.air_quality.results[0].measurements[0];
+      document.getElementById('summary').textContent = weather.summary;
+      document.getElementById('temp').textContent = weather.temperature;
+      document.getElementById('aq_parameter').textContent = air.parameter;
+      document.getElementById('aq_value').textContent = air.value;
+      document.getElementById('aq_units').textContent = air.unit;
+      document.getElementById('aq_date').textContent = air.lastUpdated;
+    } catch (error) {
+      console.error(error);
+      air = { value: -1 };
+      document.getElementById('aq_value').textContent = 'NO READING';
+    }
+
+    const data = { lat, lon, weather, air };
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+    const db_response = await fetch('/api', options);
+    const db_json = await db_response.json();
+    console.log(db_json);
+  });
+} else {
+  console.log('geolocation not available');
+}
