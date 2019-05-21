1ab54f8b9a37b19d61798b5f15c739f517ec111e
Daniel Shiffman <daniel@shiffman.net>
2019-05-21 18:49:43 -0400

    port from environment variable

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 const app = express();
-app.listen(3000, () =>
-  console.log('Starting server: http://localhost:3000')
-);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Starting server at ${port}`);
+});
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
